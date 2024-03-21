f470e68116eb949a4eabc348aa50b73436bbf661
lee sua <s2332@e-mirim.hs.kr>
2024-03-21 11:45:23 +0900

    02-01 c++ 기본예제

---
+++ 2024cpp22/02-01 기본예제.cpp
@@ -0,0 +1,6 @@
+﻿#include <iostream>
+int main() {
+	//cout : 출력객체 endl : 개행객체
+	std::cout << "hello world!"<<std::endl;
+	return 0;
+}
--- 2024cpp22/02-01.cpp
+++
@@ -1,6 +0,0 @@
-﻿#include <stdio.h>
-main() {
-	//집가고 싶다집가고시ㅍ다
-	printf("집 가고 싶다.\n");
-	return 0;
-}

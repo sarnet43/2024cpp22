62c996e4d9d5d6d9cc00e1144d632094924dd616
lee sua <s2332@e-mirim.hs.kr>
2024-03-21 11:56:53 +0900

    02-01 이름공간

--- 2024cpp22/02-01 기본예제.cpp
+++ 2024cpp22/02-01 기본예제.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
+//이름공간, std::을 안해도 된다.
+using namespace std;
 int main() {
 	//cout : 출력객체 endl : 개행객체
-	std::cout << "hello world!"<<std::endl;
+	cout << "hello world!"<<endl;
 	return 0;
 }

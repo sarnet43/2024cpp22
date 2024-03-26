f1d9919216afba55e72803538c9d38da20633e17
lee sua <s2332@e-mirim.hs.kr>
2024-03-26 16:09:49 +0900

    같은 결과2

--- 2024cpp22/배열과포인터.cpp
+++ 2024cpp22/배열과포인터.cpp
@@ -4,8 +4,8 @@ int main() {
 	int arr[4] = {1,2,3};
 	char str[4] = "asd";
 
-	printf("%d  %d\n", &arr[0], &arr[1]); //주소값 4바이트만큼의 차이
-	printf("%d  %d\n", arr+0, arr+1); 
+	printf("%d  %d\n", arr[0], arr[1]); //주소값 4바이트만큼의 차이
+	printf("%d  %d\n", *(arr + 0),  *(arr + 1));
 
 	return 0;
 }	

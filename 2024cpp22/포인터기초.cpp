49da648fd87f1ea8a61214bd0e2d92b1319d7308
lee sua <s2332@e-mirim.hs.kr>
2024-03-26 15:00:20 +0900

    배열의 이름은 시작주소를 나타낸다

---
+++ 2024cpp22/포인터기초.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+//이름공간, std::을 안해도 된다.
+using namespace std;
+int main() {
+	int num = 3;
+	cout << num << endl; //3 (변수에 들어있는 값)
+	cout << &num << endl; //변수에 메모리 공간에 할당 되어있는 주소
+
+	int arr[5] = { 10,20,30,40,50 };
+	cout << arr << endl; //배열이 메모리 공간에 할당되어있는 주소
+
+}

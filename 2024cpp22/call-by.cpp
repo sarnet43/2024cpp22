6e9dfb81c25c7f0a7567b30200e6a6e0db1b7c40
lee sua <s2332@e-mirim.hs.kr>
2024-03-26 15:33:03 +0900

    call_by_value

---
+++ 2024cpp22/call-by.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+//이름공간, std::을 안해도 된다.
+using namespace std;
+void call_by_value(int val) {
+	//새로운 지역변수 val을 만들고 거기에 10을 넣는다
+	val = 10;
+}
+int main() {
+	int num = 4; 
+	//num의 값은 바뀌지 않는다
+	call_by_value(num);
+	cout << num;
+	return 0;
+}	

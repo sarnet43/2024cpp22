fefe0cd6a054ad525a911ead9dcd559b43700add
lee sua <s2332@e-mirim.hs.kr>
2024-03-28 11:08:11 +0900

    포인터 연산

--- 2024cpp22/배열과포인터.cpp
+++ 2024cpp22/배열과포인터.cpp
@@ -4,8 +4,20 @@ int main() {
 	int arr[4] = {1,2,3};
 	char str[4] = "asd";
 
-	printf("%d  %d\n", arr[0], arr[1]); //주소값 4바이트만큼의 차이
+	printf("%d  %d\n", arr[0], arr[1]); 
 	printf("%d  %d\n", *(arr + 0),  *(arr + 1));
+
+	int i = 1;
+	char c = 'b';
+	double d = '3.0';
+
+	int* pi = &i;
+	char* pc = &c;
+	double* pd = &d;
+	//각각 4, 1, 8 차이
+	printf("%d %d %d\n",pi,pc,pd);
+	printf("%d %d %d\n", pi+1, pc+1, pd+1);
+	
 
 	return 0;
 }	

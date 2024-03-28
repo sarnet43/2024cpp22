168e325ec64a31a70b613afd036cffaa7e0610dd
lee sua <s2332@e-mirim.hs.kr>
2024-03-28 13:11:17 +0900

    함수를 통해 배열의 값을 바꾸기 by 포인터

---
+++ 2024cpp22/배열과포인터그리고함수.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+//배열의 모든 요소를 0으로 바꿔주는 함수
+void erase(int* ptr, int length) {
+	for (int i = 0; i <length; i++) {
+		ptr[i] = 0;
+		//*(ptr+i) = 0;
+	}
+}
+int main() {
+	int arr[4] = {1,2,3,4};
+	int length = sizeof(arr) / sizeof(arr[0]);
+	erase(arr, length);
+	for (int i = 0; i < 4; i++) printf("%d\n", arr[i]);//배열의 모든 요소를 출력
+	return 0;
+}	

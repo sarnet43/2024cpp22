4168cfa35d422d8dd0514678e1a9543208c6c743
lee sua <s2332@e-mirim.hs.kr>
2024-04-04 11:16:33 +0900

    string 대입1(C언어 방식)

---
+++ 2024cpp22/02-03 string.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	char str[30] = "develop hungry";
+	char str2[30];
+	int i = 0;
+	//str에 있는 내용을 str2에 복사.
+	//'\0'을 만날때까지
+	for (; str[i] != '\0'; i++) 
+		str2[i] = str[i];
+	str2[i] = str[i]; //널문자 ('\0')복사 
+
+	printf("%s", str2);
+
+}

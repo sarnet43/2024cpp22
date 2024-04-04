eeb89253ff252c7404d2eeb5aaa31efa29d0170b
lee sua <s2332@e-mirim.hs.kr>
2024-04-04 11:56:40 +0900

    string 문자열추가(C++방식)

--- 2024cpp22/02-04 string문자추가.cpp
+++ 2024cpp22/02-04 string문자추가.cpp
@@ -1,11 +1,12 @@
-﻿#include <stdio.h>
-#include <string.h>
+﻿#include <iostream>
+#include <string>
+using namespace std;
 int main(void) {
 	//기존 문자열에다가 추가
-	char str[30] = "c";
-	strcat(str, "add");
+	string str = "c++";
+	str += "addition";
 
-	printf("%s",str);
+	cout << str;
 
 
 }

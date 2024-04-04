65052f7fbce011e928783a6afab32f18de711077
lee sua <s2332@e-mirim.hs.kr>
2024-04-04 11:42:47 +0900

    string 대입2 (C언어방식2)

--- 2024cpp22/02-03 string대입(복사).cpp
+++ 2024cpp22/02-03 string대입(복사).cpp
@@ -1,14 +1,12 @@
 ﻿#include <stdio.h>
-#
+#include <string.h>
 int main(void) {
 	char str[30] = "develop hungry";
 	char str2[30];
 	int i = 0;
 	//str에 있는 내용을 str2에 복사.
 	//'\0'을 만날때까지
-	for (; str[i] != '\0'; i++) 
-		str2[i] = str[i];
-	str2[i] = str[i]; //널문자 ('\0')복사 
+	strcpy(str2, str); //보안 취약
 
 	printf("%s", str2);
 

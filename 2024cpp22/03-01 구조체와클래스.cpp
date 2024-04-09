6bc14dbcce1a1b9295a5ddb223483e9bb331b566
lee sua <s2332@e-mirim.hs.kr>
2024-04-09 15:51:06 +0900

    생성자

--- 2024cpp22/03-01 구조체와클래스.cpp
+++ 2024cpp22/03-01 구조체와클래스.cpp
@@ -1,17 +1,21 @@
 ﻿#include <iostream>
 #include <string>
 using namespace std;
-struct Student {
-
+class Student {
+public: 
+	//생성자 : 객체가 생성될 때 호출되는 함수
+	//생성자는 반환형이 없다. 
+	Student(int hakbun, string name) {
+		hakbun_ = hakbun;
+		name_ = name;
+	}
+private:
 	int hakbun_;
 	string name_;
 
  };
 int main(void) {
-	Student jisu;
-	//구조체는 default가 pubic이다. 
-	jisu.hakbun_ = 2202;
-	jisu.name_ = "나지수";
+	Student jisu = Student(2202, "나지수");
 
 	return 0;
 

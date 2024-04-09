9615df519fad8da12c9657fa02ac5710d304088d
lee sua <s2332@e-mirim.hs.kr>
2024-04-09 15:39:31 +0900

    클래스는 default가 private

---
+++ 2024cpp22/03-01 구조체와클래스.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+class Student {
+	int hakbun_;
+	string name_;
+
+ };
+int main(void) {
+	Student jisu;
+	//클래스는 default가 private이다. 
+	jisu.hakbun_ = 2202;
+	jisu.name_ = "나지수";
+
+	return 0;
+
+}

431da931750ef87c8f144cc9af6cad8b5dfae5da
lee sua <s2332@e-mirim.hs.kr>
2024-04-11 10:46:13 +0900

    03-01 생성자 오버로딩

--- 2024cpp22/03-01 구조체와클래스.cpp
+++ 2024cpp22/03-01 구조체와클래스.cpp
@@ -5,6 +5,10 @@ class Student {
 public: 
 	//생성자 : 객체가 생성될 때 호출되는 함수
 	//생성자는 반환형이 없다. 
+	Student() {
+		hakbun_ = 2222;
+		name_ = "이미림";
+	}
 	Student(int hakbun, string name) {
 		hakbun_ = hakbun;
 		name_ = name;
@@ -12,10 +16,11 @@ public:
 private:
 	int hakbun_;
 	string name_;
-
+	
  };
 int main(void) {
 	Student jisu = Student(2202, "나지수");
+	Student mirim = Student();
 
 	return 0;
 

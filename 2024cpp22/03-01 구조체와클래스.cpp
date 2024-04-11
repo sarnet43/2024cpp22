9f6cba064d74a0940b09386b390fbebbf31c9d49
lee sua <s2332@e-mirim.hs.kr>
2024-04-11 11:38:32 +0900

    this : 자기자신을 가리키는 포인터

--- 2024cpp22/03-01 구조체와클래스.cpp
+++ 2024cpp22/03-01 구조체와클래스.cpp
@@ -6,7 +6,10 @@ public:
 	//생성자 : 객체가 생성될 때 호출되는 함수
 	//생성자는 반환형이 없다. 
 	Student() {
-		hakbun_ = 2222;
+		//this : 자기 자신을 가리키는 포인터
+		//멤버를 명확하게 가리킬 수 있다.
+
+		this->hakbun_ = 2222;
 		name_ = "이미림";
 	}
 	Student(int hakbun, string name) {

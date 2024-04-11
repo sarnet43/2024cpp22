63261e91bdf5d6ad279ca5505b8dcac65b95d2fd
lee sua <s2332@e-mirim.hs.kr>
2024-04-11 12:08:53 +0900

    03-01 객체 배열 동적할당 해체

--- 2024cpp22/03-01 구조체와클래스.cpp
+++ 2024cpp22/03-01 구조체와클래스.cpp
@@ -28,15 +28,18 @@ private:
  };
 int main(void) {
 	//동적할당 : 메모리는 heap에서 할당, 실행(런타임)시 메모리 크기가 정해짐
-	Student* jisu = new Student(2202, "나지수");
+	Student* stu = new Student[3]{
+		{2202,"나지수"},
+		{2203, "박지영"},
+		{2215, "추다율"}
+	};
 
-	//정적할당 : 메모리는 stack할당 , 컴파일시 메모리 크기가 정해짐
-	Student mirim = Student();
-	jisu->show();
-	mirim.show();
+	for (int i = 0; i < 3; i++) {
+		stu[i].show();
+	}
 
-	//동적할당 해제
-	delete jisu;
+	//배열의 동적할당 해제
+	delete []stu;
 	return 0;
 
 }

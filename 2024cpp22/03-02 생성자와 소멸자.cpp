7906650dfba4faa88a8d38168a2e3f9d612b61da
lee sua <s2332@e-mirim.hs.kr>
2024-04-18 11:42:09 +0900

    03-02 생성자, 소멸자 호출시기 확인

--- 2024cpp22/03-02 생성자와 소멸자.cpp
+++ 2024cpp22/03-02 생성자와 소멸자.cpp
@@ -9,12 +9,13 @@ public:
 		int length = strlen(name);
 		//'\0'가 들어가기 위해 공간 하나 더 추가
 		name_ = new char[length+1];
-		
 		strcpy(name_, name);
+		cout << "생성자 호출 완료 " << endl;
 	}
 	//소멸자 : 객체가 소멸할 때 (메모리에서 지워질 때) 호출되는 함수
 	~Student(void) {
 		delete []name_;
+		cout << "소멸자 호출 완료" << endl;
 	}
 	void show(void) {
 		cout << hakbun_<< " ";
@@ -27,9 +28,9 @@ private:
  };
 int main(void) {
 	Student* stu = new Student(2222, "이미림");
-	stu -> show();
+	cout << "I'm still hungry- 거스 히딩크" << endl;
 	delete stu;
-
+	cout << "It ain't over, till it's over - 요기배라" << endl;
 	return 0;
 
 }

688e3d73498bce44a8f6ee0b7af8dfdc47381a70
lee sua <s2332@e-mirim.hs.kr>
2024-08-20 15:24:32 +0900

    동적할당 복습

---
+++ 2024cpp22/05-03 다형성.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal {
+public:
+	void walk(void) {
+		cout << "걷다" << endl;
+	}
+	void bark(void) {
+		cout << "짖다" << endl;
+	}
+	void eat(void) {
+		cout << "먹다" << endl;
+	}
+private:
+	string name_;
+	unsigned int age_;
+	int leg_num_;
+
+};
+
+void main(void) {
+	Animal *animal = new Animal();
+	animal->bark();
+	animal->walk();
+	animal->eat();
+
+	delete animal;
+}

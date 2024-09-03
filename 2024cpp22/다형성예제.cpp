4889b073d8b3a699e87eb2c2f5464dbd87f94033
lee sua <s2332@e-mirim.hs.kr>
2024-09-03 15:48:51 +0900

    프런트창

--- 2024cpp22/다형성예제.cpp
+++ 2024cpp22/다형성예제.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <string>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -78,10 +79,47 @@ void main(void) {
 	Food* player = new Kimchi(15, 95, "갓김치", 86, 100, 100);
 	Food* friends = new Cheese(100, 20, "짜게치", 100, 20, 100);
 
-	player->show();
-	cout << endl << endl;
-	friends->show();
+	while (true) {
+		cout << "☆나★" << endl;
+		player->show();
+		cout << endl << endl;
+		cout<<"☆상대방★" << endl;
+		friends->show();
 
+		cout << "----------------------------------------" << endl;
+
+		int select;
+		cout << "1. 공격" << endl;
+		cout << "2. 특수1" << endl;
+		cout << "3. 특수2" << endl;
+		cout << "4. 방어" << endl;
+
+		cin >> select;
+		switch (select)
+		{
+		case 1:
+			//TO DO : 공격
+			cout << "공격" << endl;
+			break;
+		case 2:
+			//TO DO : 특수1
+			cout << "특수1" << endl;
+			break;
+		case 3:
+			//TO DO : 특수2
+			cout << "특수2" << endl;
+			break;
+		case 4:
+			cout << "방어" << endl;
+			//TO DO : 방어
+			break;
+		
+		default:
+			cout << "디폴트" << endl;
+		}
+		system("pause");
+	}
+	
 	delete friends;
 	delete player;
 }

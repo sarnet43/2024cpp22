f8c9c415db3d0ae3322dec9a6a989c76965d143e
lee sua <s2332@e-mirim.hs.kr>
2024-09-03 13:57:12 +0900

    다형성 적용 전

--- 2024cpp22/다형성예제.cpp
+++ 2024cpp22/다형성예제.cpp
@@ -9,6 +9,12 @@ public:
 		: civil_(civil), force_(force), name_(name), territory_(territory) {
 
 	}
+	void show() {
+		cout << "국민 수 : " << civil_ << endl;
+		cout << "군력 : " << force_ << endl;
+		cout << "이름 :" <<name_ << endl;
+		cout << "면적 : " << territory_ << endl;
+	}
 private:
 	int civil_; //국민 수
 	int force_; //군력
@@ -23,17 +29,26 @@ public:
 		{
 
 	}
+
+	void show() {
+		cout << "마늘 : " << garic_ << endl;
+		cout << "고추 : " << pepper_ << endl;
+	}
 private:
 	int garic_;
 	int pepper_; 
 };
 
 class Jelly : public Food {
-public:
+public: 
 	Jelly (int civil, int force, string name, int territory, int gelatin, int sugar)
 		: Food(civil, force, name, territory), gelatin_(gelatin), sugar_(sugar)
 	{
 
+	}
+	void show() {
+		cout << "젤라틴 : " << gelatin_ << endl;
+		cout << "설탕 : " << sugar_ << endl;
 	}
 private:
 	int gelatin_;
@@ -47,7 +62,23 @@ public:
 		: Food(civil, force, name, territory), milk_(milk), rennet_(rennet) {
 
 	}
+	void show() {
+		cout << "우유 : " << milk_ << endl;
+		cout << "응고제 : " << rennet_ << endl;
+	}
 private:
 	int milk_;
 	int rennet_; //응고제
 };
+
+void main(void) {
+	Food* player = new Kimchi(15, 95, "갓김치", 86, 100, 100);
+	Food* friends = new Cheese(100, 20, "짜게치", 100, 20, 100);
+
+	player->show();
+	cout << endl << endl;
+	friends->show();
+
+	delete friends;
+	delete player;
+}

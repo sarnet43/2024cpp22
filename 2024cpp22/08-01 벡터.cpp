d1dbc270e9a0a1c84a05fe812425fec5c0807ab2
lee sua <s2332@e-mirim.hs.kr>
2024-09-24 15:58:47 +0900

    반복자 (iterator)

--- 2024cpp22/08-01 벡터.cpp
+++ 2024cpp22/08-01 벡터.cpp
@@ -9,6 +9,7 @@ void main(void) {
 	//미리 공간을 많이 확보하자, 성능을 위해
 	v.reserve(100);
 
+
 	//벡터 추가
 	v.push_back(4);
 	v.push_back(6);
@@ -20,7 +21,7 @@ void main(void) {
 	//값 접근
 	cout << v[0] << endl;
 	cout << v.at(1) << endl;
-
+	
 	//값 수정
 	cout << v[5] << endl;
 	v[5] = 500000000;
@@ -35,6 +36,8 @@ void main(void) {
 	v.pop_back();
 
 	cout << endl;
-	for (int i = 0; i < v.size(); i++)
-		cout << v[i] << endl;
+	//반복자 (iterator)
+	vector<long long>::iterator iter;
+	for (iter = v.begin(); iter != v.end(); iter++)
+		cout << *iter << endl;
 }

bed6448b032f0d3bf57e7e0108621c604d825b5a
lee sua <s2332@e-mirim.hs.kr>
2024-09-24 15:48:32 +0900

    08-02 map

---
+++ 2024cpp22/08-02 맵.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+void main(void) {
+	map<string, long long> money;
+
+	//맵 추가
+	money["지수"] = 100000000;
+	money["수빈"] = 300000000;
+	money["민선"] = 100;
+
+	//반복자
+	map<string, long long>::iterator iter;
+	for (iter = money.begin(); iter != money.end(); iter++)
+		cout << iter->first << "는 " << iter->second << endl;
+
+	//값 수정
+	money["민선"] = 50000000;
+	cout << money["민선"] << endl;
+
+	//크기
+	cout << money.size() << endl;
+
+}

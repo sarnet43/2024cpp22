fabc5e56e4f13348a843692b19b9906930ace861
lee sua <s2332@e-mirim.hs.kr>
2024-09-26 11:01:35 +0900

    09-01 enum 적용 전

---
+++ 2024cpp22/09-01 enum.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+void main(void) {
+	
+	//0 : 한식, 1 : 일식, 2 : 중식
+	int style = 0;
+	
+	switch (style)
+	{
+	case 0:
+		cout << "김치, 쌈, 비빔밥" << endl;
+		break;
+	case 1:
+		cout << "초밥, 타코야끼" << endl;
+		break;
+	case 2:
+		cout << "동파육, 마라탕후루" << endl;
+	default:
+		break;
+	}
+}

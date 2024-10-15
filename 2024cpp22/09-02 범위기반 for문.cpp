ac9b7ab6359ddbe6aaba168aabf0065bf82d7aaf
lee sua <s2332@e-mirim.hs.kr>
2024-10-15 14:39:01 +0900

    09-03 auto

--- 2024cpp22/09-02 범위기반 for문.cpp
+++ 2024cpp22/09-02 범위기반 for문.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 
 int main(void) {
-    vector<int> v = { 1,2,3,4,5 };
+    vector<int> v;
+    v.push_back(12);
+    v.push_back(20);
+    v.push_back(34);
+    v.push_back(65);
+    v.push_back(87);
 
     for (int idx : v) {
         cout << idx << endl;
---
+++ 2024cpp22/09-03 auto.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    vector<string> v = { "롯데관광", "동화면세점", "제주드림타워", "마이데일리"};
+
+    for (auto str : v) {
+        cout << str << endl;
+    }
+
+
+    return 0;
+}

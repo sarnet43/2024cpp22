5cf7e368ef822d7d478412d7118603fa7783dff3
lee sua <s2332@e-mirim.hs.kr>
2024-10-19 16:28:55 +0900

    [sG] 플레이어 띄우기

--- shootingG/main.cpp
+++ shootingG/main.cpp
@@ -1,1 +1,23 @@
-﻿
+﻿#include <SFML/Graphics.hpp>
+using namespace sf;
+using namespace std;
+void main(void)
+{
+	RenderWindow window(VideoMode(1200, 700), "shootingG");
+	while (window.isOpen()) {
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		RectangleShape player;
+		player.setSize(Vector2f(181.f, 75.f));
+		player.setFillColor(Color::Magenta);
+		player.setPosition(500.f, 400.f);
+		window.clear();
+		window.draw(player);
+		window.display();
+	}
+}
